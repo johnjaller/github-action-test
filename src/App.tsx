2596327a3b15d575433b3b84b5bbec47748ae655
johnjaller <johnjaller@hotmail.com>
2025-01-26 00:37:50 +0800

    fix: fix build error

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React bye</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          {/* count is {count} hi */}
+          count is {count} hi
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR

bcbde6646a84b9a8ac17c3416a93dedc0902762e
johnjaller <johnjaller@hotmail.com>
2025-01-25 18:12:26 +0800

    fix: fix something

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,6 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p></p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
